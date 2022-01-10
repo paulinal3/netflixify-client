45615c5e0e8ddf4fd57e80240c43a56ce00d122c
paulinal3 <paulinal3@outlook.com>
2022-01-10 15:48:05 -0500

    creating axios calls for all playlist routes

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -10,3 +10,53 @@ export const getPlaylists = (user) => {
         }
     })
 }
+
+export const getOnePlaylist = (user, playlistId) => {
+    return axios({
+        method: 'GET',
+        url: `${apiUrl}/playlists/${playlistId}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+    })
+}
+
+export const postPlaylist = (user, newPlaylist) => {
+    return axios({
+        method: 'GET',
+        url: `${apiUrl}/playlists`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        },
+        data: {
+            playlist: {
+                title: newPlaylist
+            }
+        }
+    })
+}
+
+export const updatePlaylist = (user, playlistId, changePlaylist) => {
+    return axios({
+        method: 'POST',
+        url: `${apiUrl}/playlists/${playlistId}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        },
+        data: {
+            playlist: {
+                title: changePlaylist
+            }
+        }
+    })
+}
+
+export const destroyPlaylist = (user, playlistId) => {
+    return axios({
+        method: 'DELETE',
+        url: `${apiUrl}/playlists/${playlistId}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+    })
+}
