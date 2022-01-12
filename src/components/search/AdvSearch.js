a8fe65e4838eae5726403fbcd118d3f5b55845ea
paulinal3 <paulinal3@outlook.com>
2022-01-12 09:53:00 -0500

    handle click succesfully toggling for adv search

--- src/components/search/AdvSearch.js
+++ src/components/search/AdvSearch.js
@@ -2,7 +2,12 @@ import { Form, Button } from "react-bootstrap"
 
 export default function AdvSearch(props) {
 
+    const advSearchClick = (e) => {
+        props.setAdvancedSearch(!props.advancedSearch)
+    }
+    // conditional to display advanced search options
     const displayAdvSearch = props.advancedSearch === true ? (
+        // if true, display checkbox options
         <div key={`inline-$checkbox`} className="mb-3">
             <Form.Check
                 inline
@@ -24,37 +29,16 @@ export default function AdvSearch(props) {
                 type='checkbox'
                 id={`inline-$checkbox-3`}
             />
+            <Button onClick={advSearchClick}>Close</Button>
         </div>
+        // if false, display button
     ) : (
-        <Button>Advanced Search</Button>
+        <Button onClick={advSearchClick}>Advanced Search</Button>
     )
     
     return (
         <div>
             {displayAdvSearch}
-            {/* <Button>Advanced Search</Button> */}
-            {/* <div key={`inline-$checkbox`} className="mb-3">
-                <Form.Check
-                    inline
-                    label="Type"
-                    name="group1"
-                    type='checkbox'
-                    id={`inline-$checkbox-1`}
-                />
-                <Form.Check
-                    inline
-                    label="Release Date"
-                    name="group1"
-                    type='checkbox'
-                    id={`inline-$checkbox-2`}
-                />
-                <Form.Check
-                    inline
-                    label="Subtitles"
-                    type='checkbox'
-                    id={`inline-$checkbox-3`}
-                />
-            </div> */}
         </div>
     )
 }
