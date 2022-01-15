ceebaddef87ca999cbb81083d2fa9e5daa229ee1
paulinal3 <paulinal3@outlook.com>
2022-01-15 16:38:04 -0500

    removing extra / from production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://netflixify-server.herokuapp.com/',
+	production: 'https://netflixify-server.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
