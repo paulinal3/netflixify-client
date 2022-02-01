cc9700af89978bc5e7f16bc0fa8c0445bb611def
paulinal3 <paulinal3@outlook.com>
2022-01-31 19:28:18 -0500

    updating logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import Sortable from 'sortablejs/modular/sortable.complete.esm.js'
 
 const Home = (props) => {
-	// const { msgAlert, user } = props
 	console.log('props in home', props)
-	
-	// let el = document.getElementById('listWithHandle')
-
-	// Sortable.create(el, {
-	// 	handle: '.glyphicon-move',
-	// 	animation: 150
-	//   });
-
 
 	return (
 		<div id='homePage'>
-			<h1 id='appName'>NETFLIXIFY</h1>
+			{/* <h1 id='appName'>NETFLIXIFY</h1> */}
+			<div>
+				<h1>Search</h1>
+			</div>
+			<div>
+				<h1>Add</h1>
+			</div>
+			<div>
+				<h1>Watch</h1>
+			</div>
 		</div>
 	)
 }
