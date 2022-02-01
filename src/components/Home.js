15833cc3082e526251abbf8dec81b0f116bf05b9
paulinal3 <paulinal3@outlook.com>
2022-01-31 23:05:34 -0500

    styling and formatting landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
-import Sortable from 'sortablejs/modular/sortable.complete.esm.js'
+import {BsSearch, BsPlayCircle} from "react-icons/bs"
+import {MdOutlineAddCircleOutline} from "react-icons/md"
 
 const Home = (props) => {
 	console.log('props in home', props)
 
 	return (
 		<div id='homePage'>
-			{/* <h1 id='appName'>NETFLIXIFY</h1> */}
-			<div>
-				<h1>Search</h1>
-			</div>
-			<div>
-				<h1>Add</h1>
-			</div>
-			<div>
-				<h1>Watch</h1>
+			<h1 id='appName'>NETFLIXIFY</h1>
+			<div id='homeContainer'>
+				<div class='landingPage'>
+					<h3><BsSearch size='25%'/></h3>
+					<hr />
+					<h5>Search over 17000 Netflix movies and shows across 38 different countries</h5>
+				</div>
+				<div class='landingPage'>
+					<h3><MdOutlineAddCircleOutline size='25%' /></h3>
+					<hr />
+					<h5>Create your own playlists and add as many videos as you choose!</h5>
+				</div>
+				<div class='landingPage'>
+					<h3><BsPlayCircle size='25%'/></h3>
+					<hr />
+					<h5>Be directed straight to Netflix based off the show or movie you want to watch.</h5>
+				</div>
 			</div>
 		</div>
 	)
