c355781e45d81bef1b13774713216e108d3dc860
paulinal3 <paulinal3@outlook.com>
2022-03-05 18:59:13 -0500

    profile page fully responsive

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,8 +3,10 @@ import { Button, Card, Form, OverlayTrigger, Tooltip } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 import { getPlaylists } from "../../api/playlist"
+
 import IndexPlaylist from "./playlist/Index/IndexPlaylist"
 import NewPlaylist from "./playlist/NewPlaylist"
+
 import { GrAdd } from "react-icons/gr"
 import { RiLockPasswordFill } from "react-icons/ri"
 
@@ -12,31 +14,30 @@ export default function Profile(props) {
 
     // const [playlist, setPlaylist] = useState([])
     const [modalShow, setModalShow] = useState(false)
-    const [playlistsOrder, setPlaylistsOrder] = useState('alph')
+    const [playlistsOrder, setPlaylistsOrder] = useState("alph")
 
-    // render all the user's playlists as soon as they sign in
+    // render all the user"s playlists as soon as they sign in
     useEffect(() => {
-        // axios call to find all the user's playlists in the db
+        // axios call to find all the user"s playlists in the db
         getPlaylists(props.user)
             .then(foundPlaylists => {
-                console.log(`these are all the current user's playlists \n`, foundPlaylists.data.foundPlaylists)
+                console.log(`these are all the current user"s playlists \n`, foundPlaylists.data.foundPlaylists)
                 // sets the playlists to state
                 props.setPlaylists(foundPlaylists.data.foundPlaylists)
-                // props.playlists.push({ title: 'Watched Videos'})
+                // props.playlists.push({ title: "Watched Videos"})
             })
             .catch(err => console.error)
     }, [])
 
     // helper method to determine which playlist is being clicked
     const playlistClicked = (e) => {
-        console.log('this playlist id was selected:\n', e.target)
-        // sets the clicked playlist's id to state
+        console.log("this playlist id was selected:\n", e.target)
+        // sets the clicked playlist"s id to state
         props.setPlaylists(e.target.value)
     }
 
     // sort the playlists alphabeticaly
     let allPlaylists = props.playlists.sort((a, b) => (a.title < b.title) ? -1 : 1)
-        // then map through them
         .map(p => {
             return (
                 // and pass them as a prop to IndexPlaylist
@@ -44,7 +45,7 @@ export default function Profile(props) {
             )
         })
 
-    // sort the playlists newest first
+    // sort the playlists newest -> oldest
     const newestPlaylists = props.playlists.sort((a, b) => {
         return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
     }).reverse().map(p => {
@@ -54,7 +55,7 @@ export default function Profile(props) {
         )
     })
 
-    // sort the playlists oldest first
+    // sort the playlists oldest -> newest
     const oldestPlaylists = props.playlists.sort((a, b) => {
         return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
     }).map(p => {
@@ -75,12 +76,16 @@ export default function Profile(props) {
     })
 
     // conditional for playlist sort
-    if (playlistsOrder === 'new') {
-        allPlaylists = newestPlaylists
-    } else if (playlistsOrder === 'old') {
-        allPlaylists = oldestPlaylists
-    } else if (playlistsOrder === 'updated') {
-        allPlaylists = updatedPlaylists
+    switch (playlistsOrder) {
+        case "new":
+            allPlaylists = newestPlaylists;
+            break;
+        case "old":
+            allPlaylists = oldestPlaylists;
+            break;
+        case "updated":
+            allPlaylists = updatedPlaylists;
+            break;
     }
 
     const sortClicked = (e) => {
@@ -100,39 +105,88 @@ export default function Profile(props) {
     )
 
     return (
-        <div id='container'>
-            <div id='profileSetting'>
-                {/* <-- playlist sort dropdown --> */}
-                <Form.Select id='sortBy' onChange={sortClicked}>
-                    <option value='alph'>A to Z</option>
-                    <option value='new'>Newest Created</option>
-                    <option value="old">Oldest Created</option>
-                    <option value='updated'>Most Updated</option>
-                </Form.Select>
-                {/* <-- create new playlist button --> */}
-                <OverlayTrigger
-                    placement="top"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={createHover}
-                >
-                    <Button size='sm' id='createBtn'><GrAdd onClick={() => setModalShow(true)} /></Button>
-                </OverlayTrigger>
-                {/* <-- change password button --> */}
-                <OverlayTrigger
-                    placement="top"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={pwHover}
-                >
-                    <Link to='../change-password'><Button size='sm' id='changePwBtn'><RiLockPasswordFill /></Button></Link>
-                </OverlayTrigger>
-                
+        // <div className="page-container" id="container">
+        //     <div id="profileSetting">
+        //         {/* <-- playlist sort dropdown --> */}
+        //         <Form.Select id="sortBy" onChange={sortClicked}>
+        //             <option value="alph">A to Z</option>
+        //             <option value="new">Newest Created</option>
+        //             <option value="old">Oldest Created</option>
+        //             <option value="updated">Most Updated</option>
+        //         </Form.Select>
+        //         {/* <-- create new playlist button --> */}
+        //         <OverlayTrigger
+        //             placement="top"
+        //             delay={{ show: 250, hide: 400 }}
+        //             overlay={createHover}
+        //         >
+        //             <Button size="sm" id="createBtn"><GrAdd onClick={() => setModalShow(true)} /></Button>
+        //         </OverlayTrigger>
+        //         {/* <-- change password button --> */}
+        //         <OverlayTrigger
+        //             placement="top"
+        //             delay={{ show: 250, hide: 400 }}
+        //             overlay={pwHover}
+        //         >
+        //             <Link to="../change-password"><Button size="sm" id="changePwBtn"><RiLockPasswordFill /></Button></Link>
+        //         </OverlayTrigger>
+
+        //     </div>
+        //     <header id="profileHeader">
+        //         <h1>{props.user.firstName}'s Playlists:</h1>
+        //     </header>
+        //     <ol id="allPlaylists">
+        //         {allPlaylists}
+        //         <Card id="watchedCard" style={{ width: "13rem" }}>
+        //             <Card.Body className="vidResModal">
+        //                 <Card.Title>Watched Videos</Card.Title>
+        //                 <Link to={`/playlists/watched`}><Button variant="success">See List</Button></Link>
+        //             </Card.Body>
+        //         </Card>
+        //     </ol>
+        //     <NewPlaylist
+        //         show={modalShow}
+        //         onHide={() => setModalShow(false)}
+        //         currentUser={props.user}
+        //         allPlaylists={props.getAllPlaylists}
+        //     />
+        // </div>
+        <div className="page-container" id="profile-page-container">
+            <div className="header-container" id="profile-header-container">
+                <header className="header-title">
+                    <h1>{props.user.firstName}'s Playlists:</h1>
+                </header>
+                <div id="profile-settings">
+                    {/* <-- playlist sort dropdown --> */}
+                    <Form.Select id="playlists-sort" onChange={sortClicked}>
+                        <option value="alph">A to Z</option>
+                        <option value="new">Newest Created</option>
+                        <option value="old">Oldest Created</option>
+                        <option value="updated">Most Updated</option>
+                    </Form.Select>
+                    <div className="profile-settings-btns">
+                        {/* <-- create new playlist button --> */}
+                        <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={createHover}
+                        >
+                            <Button size="sm" id="create-playlist-btn"><GrAdd onClick={() => setModalShow(true)} /></Button>
+                        </OverlayTrigger>
+                        {/* <-- change password button --> */}
+                        <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={pwHover}
+                        >
+                            <Link to="../change-password"><Button size="sm" id="change-pw-btn"><RiLockPasswordFill /></Button></Link>
+                        </OverlayTrigger>
+                    </div>
+                </div>
             </div>
-            <header id='profileHeader'>
-                <h1>{props.user.firstName}'s Playlists:</h1>
-            </header>
-            <ol id='allPlaylists'>
+            <ol className="cards-list" id="all-playlists-container">
                 {allPlaylists}
-                <Card id='watchedCard' style={{ width: '13rem' }}>
+                <Card id="watchedCard" style={{ width: "13rem" }}>
                     <Card.Body className="vidResModal">
                         <Card.Title>Watched Videos</Card.Title>
                         <Link to={`/playlists/watched`}><Button variant="success">See List</Button></Link>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "./css/auth.css"
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
