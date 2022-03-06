08e069713fa3f64b45d117a40f9f109a0525df64
paulinal3 <paulinal3@outlook.com>
2022-03-06 12:50:25 -0500

    show playlist page fully responsive

--- src/components/profile/playlist/Show/ShowPlaylist.js
+++ src/components/profile/playlist/Show/ShowPlaylist.js
@@ -10,12 +10,12 @@ export default function ShowPlaylist(props) {
     // grab the current url pattern
     const { pathname } = useLocation()
     // store the playlist id in the url pattern into a variable
-    const playlistId = pathname.split('/')[2]
-    // console.log('this is the playlist id:', playlistId)
+    const playlistId = pathname.split("/")[2]
+    // console.log("this is the playlist id:", playlistId)
 
     const [playlist, setPlaylist] = useState({})
     const [playlistVids, setPlaylistVids] = useState([])
-    const [playlistTitle, setPlaylistTitle] = useState('')
+    const [playlistTitle, setPlaylistTitle] = useState("")
     const [title, setTitle] = useState(true)
 
     // render the specified playlist as soon as selected by user
@@ -23,7 +23,7 @@ export default function ShowPlaylist(props) {
         // axios call to find the selected playlist in the db
         getOnePlaylist(props.user, playlistId)
             .then(foundPlaylist => {
-                console.log('this is the found playlist\n', foundPlaylist.data.playlist)
+                console.log("this is the found playlist\n", foundPlaylist.data.playlist)
                 // set the found playlist to state
                 setPlaylist(foundPlaylist.data.playlist)
                 setPlaylistVids(foundPlaylist.data.playlist.videos)
@@ -36,7 +36,7 @@ export default function ShowPlaylist(props) {
         // axios call to find the selected playlist in the db
         getOnePlaylist(props.user, playlistId)
             .then(foundPlaylist => {
-                console.log('this is the found playlist\n', foundPlaylist.data.playlist)
+                console.log("this is the found playlist\n", foundPlaylist.data.playlist)
                 // set the found playlist to state
                 setPlaylist(foundPlaylist.data.playlist)
                 setPlaylistVids(foundPlaylist.data.playlist.videos)
@@ -72,7 +72,7 @@ export default function ShowPlaylist(props) {
         // axios call
         destroyPlaylist(props.user, playlistId)
             // redirect back to profile
-            .then(() => navigate('/profile'))
+            .then(() => navigate("/profile"))
             .catch(err => console.error)
     }
 
@@ -83,22 +83,6 @@ export default function ShowPlaylist(props) {
             <IndexVideos playlistVids={v} currentUser={props.user} refreshPlaylist={refreshPlaylist} />
         )
     })
-
-    // conditional for what should be displayed
-    const displayTitle = title === true ? (
-        // if true, display title and edit button
-        <div id='editHeader'>
-            <Button id='editBtn' variant='success' className="float-end" size='sm' onClick={displayEdit}>Edit Playlist</Button>
-            <h1>{playlist.title}</h1>
-        </div>
-    ) : (
-        // if false, display text to edit title and save/delete buttons
-        <Form id='editPlaylist'>
-            <Form.Control id='titleInput' value={playlistTitle} onChange={handleTitleChange} />
-            <Button size='sm' variant='success' onClick={editPlaylist}>Save</Button>
-            <Button size='sm' variant='danger' onClick={deletePlaylist}>Delete Playlist</Button>
-        </Form>
-    )
 
     // list the playlists alphabetically
     const playlistsDropdown = props.playlists.sort((a, b) => (a.title < b.title) ? -1 : 1)
@@ -114,21 +98,46 @@ export default function ShowPlaylist(props) {
         navigate(`/playlists/${e.target.value}`)
     }
 
-    return (
-        <div id='showPlaylistPage'>
-            <header id='playlistHeader'>
-                <small>PLAYLIST</small>
-                <h1 id='showPlaylistTitle'>
-                    {displayTitle}
-                </h1>
-            </header>
-            <div id='showPlaylistSettings'>
-                <Form.Select id='playlistDropdown' onChange={playlistClicked}>
-                    <option value={playlistId}>Go to: </option>
-                    {playlistsDropdown}
-                </Form.Select>
+    const displayHeader = title ? (
+        // if true, display title and edit button
+        <header className="header-container" id="show-playlist-header">
+            <div className="header-row row-one-header">
+                <h4>PLAYLIST</h4>
+                <Button id="edit-playlist-btn" className="float-end" size="sm" onClick={displayEdit}>Edit Playlist</Button>
             </div>
-            <ol id='playlistVid'>{getPlaylistVids}</ol>
+            <div className="header-row row-two-header">
+                <h1>{playlist.title}</h1>
+                <div id="playlist-dropdown-container">
+                    <Form.Select id="playlists-dropdown" onChange={playlistClicked}>
+                        <option value={playlistId}>Go to: </option>
+                        {playlistsDropdown}
+                    </Form.Select>
+                </div>
+            </div>
+        </header>
+    ) : (
+        // if false, display text to edit title and save/delete buttons
+        <header className="header-container" id="show-playlist-header">
+            <div className="header-row row-one-header">
+                <h4>PLAYLIST</h4>
+                <Button id="edit-playlist-btn" className="float-end" size="sm" onClick={displayEdit}>Edit Playlist</Button>
+            </div>
+            <div className="header-row row-two-header">
+                <Form id="editPlaylist">
+                    <Form.Control id="edit-playlist-input" value={playlistTitle} onChange={handleTitleChange} />
+                    <Button size="sm" id="save-playlist-btn" onClick={editPlaylist}>Save</Button>
+                    <Button size="sm" id="delete-playlist-btn" onClick={deletePlaylist}>Delete Playlist</Button>
+                </Form>
+            </div>
+        </header>
+    )
+
+    return (
+        <div className="page-container" id="show-playlist-page-container">
+            {displayHeader}
+            <main>
+                <ol className="cards-list" id="playlistVid">{getPlaylistVids}</ol>
+            </main>
         </div>
     )
 }
